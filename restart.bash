24febc46c601b59cecada47eb8c13e5be13fce8b
arman756 <armanfc.enter@yahoo.com>
2024-10-22 13:44:03 +0200

    restart.bash updated

--- restart.bash
+++ restart.bash
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-container_name="Doctorise_scraper_container"
+service_name="scraper"  # Name of the service in docker-compose.yml
 
-echo "Restarting $container_name..."
-docker-compose restart scraper
+echo "Stopping and removing the $service_name service..."
+docker-compose stop $service_name
+docker-compose rm -f $service_name
+
+echo "Rebuilding and starting the $service_name service..."
+docker-compose up --build -d $service_name
