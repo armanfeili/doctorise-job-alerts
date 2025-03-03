c2d91ba04f0ba74a32f9e5f955d3c33ad819e762
arman756 <armanfc.enter@yahoo.com>
2025-03-03 17:21:52 +0100

    number of pages for nhsjobs increased to 5

--- scraper/scrape_nhs.py
+++ scraper/scrape_nhs.py
@@ -181,7 +181,7 @@ async def scrape_jobs_playwright(url, job_search_engine, category, stop_words, c
     Scrapes job postings from NHS using Playwright and BeautifulSoup,
     filtering by stop words, limiting to 2 pages, saving each job individually.
     """
-    MAX_PAGES = 3
+    MAX_PAGES = 5
     page_number = 1
 
     page = await browser.new_page()
